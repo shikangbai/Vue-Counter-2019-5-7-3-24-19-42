fc8cde4f4dc6e689ed29bdd659d788c9ede180f7
SKB <1215237417@qq.com>
2019-09-02 19:47:18 +0800

    add

---
+++ my-project/src/components/Counter.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="counter">
+    <span>{{index}}</span>
+    <button @click="add()">+</button>
+    {{count}}
+    <button @click="down()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  props: {
+    msg: String,
+    index: Number
+  },
+  methods: {
+    add: function() {
+      this.count++;
+    },
+    down: function() {
+      this.count--;
+    },
+    print() {
+      console.log("butter clicked");
+    }
+  },
+  watch: {
+    count: function(newVal, oldVal) {
+      this.$emit("listenCountChange", newVal - oldVal);
+    }
+  },
+  mounted() {
+    this.count = this.index;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+span {
+  margin-right: 20px;
+  margin-left: 20px;
+}
+</style>
--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="hello">
-    <h1>我是史康柏</h1>
-    {{count}}
-    <button @click="add()">+</button>
-    <button @click="down()">-</button>
+    <h1>click me</h1>
+    <h2>counter</h2>
+    <input type="number" v-model.number="num" />
+    <div v-for="n in getNum" v-bind:key="n">
+      <counter
+        v-bind:index="n"
+        msg="Welcome to Your Vue.js App"
+        @listenCountChange="listenCountChange"
+      />
+    </div>
+    <div>总和：{{total}}</div>
   </div>
 </template>
-
 <script>
+import counter from "./Counter";
 export default {
   name: "HelloWorld",
   data() {
     return {
-      count: 0
+      num: 0,
+      count: 0,
+      total: 0
     };
+  },
+  components: {
+    counter
   },
   props: {
     msg: String
@@ -24,11 +36,21 @@ export default {
     },
     down: function() {
       this.count--;
+    },
+    print() {
+      console.log("butter clicked");
+    },
+    listenCountChange(num) {
+      this.total += num;
+    }
+  },
+  computed: {
+    getNum: function() {
+      return this.num;
     }
   }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
@@ -42,7 +64,12 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+span {
+  margin-right: 20px;
+  margin-left: 20px;
 }
 </style>
